f248de5a46f921c2a5465c5a8daade8d92696429
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-20 22:06:28 +0530

    spacing

--- src/com/project/operators/ComparisonOperator.java
+++ src/com/project/operators/ComparisonOperator.java
@@ -9,18 +9,23 @@ public class ComparisonOperator {
 		if(value1 == value2) {
 			System.out.println("value 1 = value 2");
 		}
+		
 		if(value1 != value2) {
 			System.out.println("value 1 is not equal to value 2");
 		}
+		
 		if(value1 > value2) {
 			System.out.println("value 1 > value 2");
 		}
+		
 		if(value1 < value2) {
 			System.out.println("value 1 < value 2");
 		}
+		
 		if(value1 >= value2) {
 			System.out.println("value 1 is greater than or equal to value2");
 		}
+		
 		if(value1 <= value2) {
 			System.out.println("value 1 is less than or equal to value 2");
 		}
