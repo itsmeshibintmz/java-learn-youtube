0f9576551a30936a4787b981108d93077095a97d
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-20 21:31:43 +0530

    Unary operators

--- src/com/project/operators/UnaryOperators.java
+++ src/com/project/operators/UnaryOperators.java
@@ -3,6 +3,15 @@ package com.project.operators;
 public class UnaryOperators {
 
 	public static void main(String[] args) {
+		// Unary Operators
+		/* 
+		 * '+' : Unary Plus Operator, indicates positive value
+		 * '-' : Unary minus operator, negates an expression
+		 * '++' : Increment operator, increments a value by 1
+		 * '--' : Decrement operator, decrements a value by 1
+		 * '!' : Logical complement operator, inverts the value of a boolean
+		 */
+		
 		int result = +1;
 		System.out.println(result);
 		
