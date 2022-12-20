44273e311677f60ea99c8154fbc75f5e2ce3f537
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-20 22:22:34 +0530

    Logical,Conditonal and Relational Operatos

--- src/com/project/operators/ComparisonAndConditionalAndLogicalOperator.java
+++ src/com/project/operators/ComparisonAndConditionalAndLogicalOperator.java
@@ -1,6 +1,6 @@
 package com.project.operators;
 
-public class ComparisonOperator {
+public class ComparisonAndConditionalAndLogicalOperator {
 
 	public static void main(String[] args) {
 		int value1 = 2;
@@ -29,6 +29,16 @@ public class ComparisonOperator {
 		if(value1 <= value2) {
 			System.out.println("value 1 is less than or equal to value 2");
 		}
+		
+		// Conditional Operators
+		if((value1 == 2) && (value2 == 3)) {
+			System.out.println("value1 is 2 AND value2 is 3");
+		}if((value1 > value2) || (value1 < value2)) {
+			System.out.println("Value 1 is > value 2 OR value is < value2");
+		}
+		
+		String greater = value1 > value2 ? "value 1 is greater" : "value 2 is greater"; 
+		System.out.println(greater);
 	} 
 
 }
