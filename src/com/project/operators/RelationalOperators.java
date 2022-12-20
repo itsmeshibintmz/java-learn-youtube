ddea68abb2af0b5c966d08e0dfb10920bc994488
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-20 22:00:21 +0530

    relational operators

---
+++ src/com/project/operators/RelationalOperators.java
@@ -0,0 +1,22 @@
+package com.project.operators;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// The Equality and Relational Operators
+		/* 	==      equal to
+		 *	!=      not equal to
+		 *	>       greater than
+		 *	>=      greater than or equal to
+		 *	<       less than
+		 *	<=      less than or equal to
+		*/
+		
+		// The Conditional Operators
+		/*
+		 * && Conditional-AND
+		 * || Conditional-OR
+		*/
+	}
+
+}
