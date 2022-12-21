1468a855e686ca5c4ee646c8c7f5d4a8abf0890a
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 12:40:46 +0530

    For loop

--- src/com/project/loops/ForLoops.java
+++ src/com/project/loops/ForLoops.java
@@ -4,7 +4,8 @@ public class ForLoops {
 
 	public static void main(String[] args) {
 		int count = 10;
-		for(int i = 1;i <= count;i++) {
+		for(int i = 1;i <= count;i++) { // initialization only at once.
+			// initialization, condition, increment
 			System.out.println(i);
 		}
 	}
