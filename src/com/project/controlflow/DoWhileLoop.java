a1f325783f094eac01815bb46e322746969bec47
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 12:11:03 +0530

    DoWhile

---
+++ src/com/project/controlflow/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.project.controlflow;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int count = 10;
+		do {
+			System.out.println(count);
+			count--;
+		} while (count >= 1);
+	}
+
+}
