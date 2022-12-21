705cc3df81db6027f65b51bd03b62c4e5dde0702
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 11:24:34 +0530

    One dimensional array

---
+++ src/com/project/array/OneDimensionalArray.java
@@ -0,0 +1,19 @@
+package com.project.array;
+
+public class OneDimensionalArray {
+
+	public static void main(String[] args) {
+		int arr[] = new int[4];
+		
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		
+		System.out.println("Element at index 0 is: " + arr[0]);
+		System.out.println("Element at index 1 is: " + arr[1]);
+		System.out.println("Element at index 2 is: " + arr[2]);
+		System.out.println("Element at index 3 is: " + arr[3]);
+	}
+
+}
