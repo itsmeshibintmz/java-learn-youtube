f1a7ca3a48636c6d92e2f526efd19e24a4467736
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 12:34:51 +0530

    For Loop

---
+++ src/com/project/loops/ForLoops.java
@@ -0,0 +1,12 @@
+package com.project.loops;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		int count = 10;
+		for(int i = 1;i <= count;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
