690c1ec7350a2a6623d648365f7c943c1b5fa6e7
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 13:10:46 +0530

    conversion

--- src/com/newvideo/tutorial/Main.java
+++ src/com/newvideo/tutorial/Main.java
@@ -6,8 +6,9 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("True or False");
-		boolean scanned = sc.nextBoolean();
-		System.out.println("the answer is: "+scanned);
+		String scanned = sc.next();
+		boolean value = Boolean.parseBoolean(scanned);
+		System.out.println("the answer is: "+value);
 	}
 
 }
