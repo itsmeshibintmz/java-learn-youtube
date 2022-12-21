2d2892c3df1aa6815325c1e44819dcb8c14a0ddf
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 13:05:22 +0530

    scanner

--- src/com/newvideo/tutorial/Main.java
+++ src/com/newvideo/tutorial/Main.java
@@ -1,9 +1,13 @@
 package com.newvideo.tutorial;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int scanned = sc.nextInt();
+		System.out.println("the number is: "+scanned);
 	}
 
 }
