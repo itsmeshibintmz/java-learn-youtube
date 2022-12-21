4ea50f11d047d973ac9eb2e43c673cf0b8f65af6
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 17:31:01 +0530

    HashMap

---
+++ src/com/project/collections/HashMapDemo.java
@@ -0,0 +1,20 @@
+package com.project.collections;
+
+import java.util.*;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		// key value arrangement
+		// Class HashMap<K, V>
+		Map<String, Integer> hashMap = new HashMap<>();
+		
+		hashMap.put("a", 10);
+		hashMap.put("b", 20);
+		hashMap.put("c", 30);
+		
+		System.out.println(hashMap);
+		System.out.println("size of the hashmap is: " + hashMap.size());
+	} 
+
+}
