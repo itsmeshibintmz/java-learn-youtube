2bbc1332c78557f36a297dd538effcb7faca89e5
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 11:51:19 +0530

    Multi Dimensional Array

---
+++ src/com/project/array/MultiDimensionalArray.java
@@ -0,0 +1,11 @@
+package com.project.array;
+
+public class MultiDimensionalArray {
+
+	public static void main(String[] args) {
+		int array[][][] = { { {1,2,3},{2,3,4} }, {{3,4,5},{4,5,6}} };
+		
+		System.out.println(array[1][0][1]);
+	}
+
+}
