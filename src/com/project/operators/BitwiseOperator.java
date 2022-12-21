60062556b060c614bea16fd0fd72493c625f13db
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 10:43:50 +0530

    bitwise operator

---
+++ src/com/project/operators/BitwiseOperator.java
@@ -0,0 +1,13 @@
+package com.project.operators;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 7;
+		
+		System.out.println("a|b = " + (a | b));
+		System.out.println("a&b = " + (a & b));
+	}
+
+}
