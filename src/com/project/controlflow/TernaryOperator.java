b686dda512ff630d3a6eeab0638e26946252630a
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 10:59:16 +0530

    ternary operator

---
+++ src/com/project/controlflow/TernaryOperator.java
@@ -0,0 +1,14 @@
+package com.project.controlflow;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int result;
+		
+		result = a > b ? a : b;
+		System.out.println("Greater value is: " + result);
+	}
+
+}
