6f1b3cab17ccc6adfea04562a128cc729d674d63
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 16:40:26 +0530

    HashSet

--- src/com/project/collections/HashSetDemo.java
+++ src/com/project/collections/HashSetDemo.java
@@ -6,10 +6,11 @@ public class HashSetDemo {
 
 	public static void main(String[] args) {
 		HashSet<String> hashSet = new HashSet<String>();
-		Scanner sc = new Scanner(System.in);
 		
-		hashSet.add("A");
-		hashSet.add("A"); // will be ignored by HashSet as it is a duplicate.
+		boolean r1 = hashSet.add("A");
+		System.out.println(r1);
+		boolean r2 = hashSet.add("A"); // will be ignored by HashSet as it is a duplicate.
+		System.out.println(r2);
 		hashSet.add("B");
 		hashSet.add("C");
 		
@@ -17,7 +18,13 @@ public class HashSetDemo {
 		// [A, B, C]
 		
 		System.out.println(hashSet.isEmpty());
-
+		hashSet.remove("B");
+		System.out.println(hashSet);
+		System.out.println("A contain in the HashSet? "+ hashSet.contains("A"));
+		
+		for(String item : hashSet) {
+			System.out.println(item);
+		}
 	}
 
 }
