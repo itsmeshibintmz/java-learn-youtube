fbf92018d96db2b5de69f69377f5eb8b34b4edf6
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2022-12-21 12:11:29 +0530

    DoWhile Loop

--- src/com/project/controlflow/DoWhileLoop.java
+++ src/com/project/controlflow/DoWhileLoop.java
@@ -3,11 +3,11 @@ package com.project.controlflow;
 public class DoWhileLoop {
 
 	public static void main(String[] args) {
-		int count = 10;
+		int count = 1;
 		do {
 			System.out.println(count);
-			count--;
-		} while (count >= 1);
+			count++;
+		} while (count <= 10);
 	}
 
 }
